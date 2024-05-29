fca4f08bb11ee58e8392e6f0d3d9ce6d615573e6
Davi Lima <davidamartesgit@Gmail.com>
2024-05-29 18:25:37 -0300

    Entidade DDD que irá mapear a respota que virá no formato Json

---
+++ Entities/DDDModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalApiDadosNacionais.Entities
+{
+    public class DDDModel
+    {
+        public string? State { get; set; }
+
+        public object[]? Cities { get; set; }
+            
+        public string? Nome { get; set; }
+    }
+}
