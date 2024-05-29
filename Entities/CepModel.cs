c4514afb1a6434930b3c13f57a0f58bf7f400d5f
Davi Lima <davidamartesgit@Gmail.com>
2024-05-29 18:24:47 -0300

    Entidade CEP que será usada no mapeamento do Json.

---
+++ Entities/CepModel.cs
@@ -0,0 +1,20 @@
+﻿namespace MinimalApiDadosNacionais.Entities
+{
+    public class CepModel
+    {
+        public string? Cep { get; set; }
+
+        public string? State {  get; set; }
+
+        public string? City { get; set; }
+
+        public string? Neighborhood { get; set; }
+
+        public string? Street { get; set; }  
+
+        public string? Service { get; set; }
+
+
+
+    }
+}
