826cb0d8651ad9612fbfa8edf6db8043b46e12fd
Davi Lima <davidamartesgit@Gmail.com>
2024-05-29 18:26:26 -0300

    Interface que contém os métodos que irá fazer a buscar de Cep, Cnpj e DDD

---
+++ Service/IDadosNacionaisService.cs
@@ -0,0 +1,14 @@
+﻿using MinimalApiDadosNacionais.Entities;
+
+namespace MinimalApiDadosNacionais.Service
+{
+    public interface IDadosNacionaisService
+    {
+        Task<CepModel> BuscarCEP(string cep);
+
+        Task<CnpjModel> BuscarCNPJ(string cep);
+
+        Task<DDDModel> BuscarDDD(string cep);
+
+    }
+}
