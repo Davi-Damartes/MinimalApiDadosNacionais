b70bc678ebdb10b0970019ba178caa5e88b9b375
Davi Lima <davidamartesgit@Gmail.com>
2024-05-29 18:25:26 -0300

    Entidade CNPJ que irá mapear a respota que virá no formato Json

---
+++ Entities/CnpjModel.cs
@@ -0,0 +1,18 @@
+﻿    namespace MinimalApiDadosNacionais.Entities
+{
+    public class CnpjModel
+    {
+        public string? Cnpj { get; set; }
+        public string? UF { get; set; }
+        public string? RazaoSocial { get; set; }
+
+        public string? NomeFantasia { get; set; }
+
+        public string? Municipio { get; set; }
+
+        public decimal Capital_social { get; set; }
+
+        public string? Data_inicio_atividade { get; set; }
+
+    }
+}
